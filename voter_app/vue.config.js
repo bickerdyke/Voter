a8a12707c5f2fb6e416b325a70c8bbeaaa0ddedf
bickerdyke <48774580+bickerdyke@users.noreply.github.com>
2024-06-18 01:55:19 +0200

    CORS-Regeln für DEV-Server angeoasst

--- voter_app/vue.config.js
+++ voter_app/vue.config.js
@@ -4,6 +4,11 @@ const webpack = require("webpack");
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
     plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
