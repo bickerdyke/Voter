46dbd1e5263243799d5e3eed56eacae286aa3ea8
bickerdyke <48774580+bickerdyke@users.noreply.github.com>
2024-07-11 12:52:50 +0200

    Placeholder mit Logo fixes Platzhalter mit Logo versehen #32

--- voter_app/src/components/voting/CreateVoting.vue
+++ voter_app/src/components/voting/CreateVoting.vue
@@ -37,13 +37,12 @@
       v-if="isEditing"
       :initialValues="initialValues"
     >
-      <!-- @todo: #32 Platzhalter mit Logo versehen -->
       <div class="row py-3">
         <div class="p-3 col-4 text-center">
           <img
             class="rounded-4 img-fluid d-block mx-auto d-fill"
             style="width: 100%; max-width: 100%; object-fit: cover"
-            src="@/assets/placeholder.png"
+            src="@/assets/placeholder.svg"
           />
         </div>
         <div class="col-8 py-3">
