769b26614b653ed1906ce0aa756289467ec185fd
bickerdyke <48774580+bickerdyke@users.noreply.github.com>
2024-07-11 18:46:24 +0200

    Temporäre Elemente für Homepage als Cards angelegt

---
+++ voter_app/src/components/BetaVersionAlert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="alert alert-danger shadow mb-5 d-print-none">
+    <h3>{{ $t("Achtung") }}</h3>
+    <p>
+      Dies ist die Testversion einer privaten Hobby-Webseite. Eingegebene Daten
+      werden regelmässig gelöscht. Die Nutzung erfolgt ohne Garantien oder
+      Zusicherungen jeglicher Art.
+    </p>
+    <hr />
+    <p>
+      This is a test-version of a personal webservice. Data entered will be
+      deleted regularly. Use at your own risk.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BetaVersionAlert",
+  computed: {},
+};
+</script>
+
+<style scoped></style>
