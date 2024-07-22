83944a8e443106678d8a0eae9c06ae715ff46ef3
bickerdyke <48774580+bickerdyke@users.noreply.github.com>
2024-07-22 15:21:38 +0200

    Hobby-Disclaimer

--- voter_app/src/components/BetaVersionAlert.vue
+++ voter_app/src/components/BetaVersionAlert.vue
@@ -2,14 +2,14 @@
   <div class="alert alert-danger shadow my-4 d-print-none">
     <h3>{{ $t("Achtung") }}</h3>
     <p>
-      Dies ist die Testversion einer privaten Hobby-Webseite. Eingegebene Daten
-      werden regelmässig gelöscht. Die Nutzung erfolgt ohne Garantie oder
-      Zusicherung jeglicher Art.
+      {{ $t("Legal.Disclaimer") }}
     </p>
     <hr />
     <p>
-      This is a test-version of a personal webservice. Data entered will be
-      deleted regularly. Use at your own risk.
+      Bug-Reports:
+      <a href="https://github.com/bickerdyke/Voter/issues"
+        >https://github.com/bickerdyke/Voter/issues</a
+      >
     </p>
   </div>
 </template>
