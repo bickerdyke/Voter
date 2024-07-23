56a2546ae71645f66f59877da1c22facadaa44a4
bickerdyke <48774580+bickerdyke@users.noreply.github.com>
2024-07-23 17:19:08 +0200

    Not Found page. Fixes 404-Not-Found Seite schreiben #57

--- voter_app/src/pages/NotFoundPage.vue
+++ voter_app/src/pages/NotFoundPage.vue
@@ -1,34 +1,55 @@
 <template>
   <TheHomeLayout>
-    <template #default>
-      <div class="container">
-        <div class="row">
-          <div class="offset-2 col-8">
-            <div class="card mt-5">
-              <div class="card-body text-center">
-                <img src="@/assets/x_notFound.svg" width="300" />
-                <div class="display-3">Seite nicht gefunden</div>
-                <hr />
-                <router-link to="/" class="btn bg-vue"
-                  >Bring mich zurück zur Startseite</router-link
-                >
-              </div>
-            </div>
+    <div>
+      <!-- Header -->
+      <div class="row">
+        <SessionHeadline>
+          <template v-slot:headline>{{ $t("Voter-Service Title") }}</template>
+          <template v-slot:subtitle>{{ $t("Voter-Service Tagline") }}</template>
+        </SessionHeadline>
+      </div>
+
+      <div class="row">
+        <div class="hero py-5 mt-2 mb-5">
+          <div class="col-5 offset-1">
+            <h3>{{ $t("NotFoundPage.pageNotFound") }}</h3>
           </div>
         </div>
+        <router-link :to="{ name: 'home' }" class="btn btn-secondary">{{
+          $t("NotFoundPage.backToHome")
+        }}</router-link>
       </div>
-    </template>
+
+      <BetaVersionAlert v-if="!$root.isDevMode" />
+      <TestLinks v-if="$root.isDevMode" />
+    </div>
   </TheHomeLayout>
 </template>
 
 <script>
 import TheHomeLayout from "@/layouts/TheHomeLayout";
+import SessionHeadline from "@/components/SessionHeadline";
+import TestLinks from "@/components/TestLinks";
+import BetaVersionAlert from "@/components/BetaVersionAlert";
+
 export default {
   name: "NotFoundPage",
   components: {
     TheHomeLayout,
+    SessionHeadline,
+    TestLinks,
+    BetaVersionAlert,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.hero {
+  width: 100vw;
+  min-height: 30vh;
+  height: auto;
+  background-image: url("@/assets/ballot_lg.jpg");
+  background-position: center;
+  background-size: cover;
+}
+</style>
