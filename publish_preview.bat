234fe10e37c83b8d5ca8d0250d93bc8e1e621e7d
bickerdyke <48774580+bickerdyke@users.noreply.github.com>
2026-02-23 03:10:16 +0100

    Batch Datei um den lokalen Stand als Preview zu veröffentlichen

--- publish_preview.bat
+++ publish_preview.bat
@@ -1,1 +1,9 @@
+REM This batch file builds the voter app and deploys it to a firebase hosting preview channel.
+
+ECHO Building voter app...
+pushd voter_app
+call npm run build
+popd
+
+ECHO Deploying to firebase hosting preview channel...
 firebase hosting:channel:deploy preview
